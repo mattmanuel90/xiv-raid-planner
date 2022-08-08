6418c9cbdb7e975734d88622d5a0efa741325f3d
Matthew Manuel <manuel.matthew90@gmail.com>
2022-08-08 10:27:37 +1000

    Added Daisy UI and some job assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold">
           Create <span className="text-purple-300">T3</span> App
         </h1>
         <p className="text-2xl text-gray-700">This stack uses:</p>
@@ -67,8 +67,8 @@ const TechnologyCard = ({
 }: TechnologyCardProps) => {
   return (
     <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
+      <h2 className="text-lg">{name}</h2>
+      <p className="text-sm">{description}</p>
       <Link href={documentation}>
         <a
           className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
--- src/pages/raid.tsx
+++ src/pages/raid.tsx
@@ -3,26 +3,38 @@ import Link from "next/link";
 
 const waymarks = ["1", "2", "3", "4"];
 
-const ComponentPanel = () => {
-    return <div />
+const LeftComponentPanel = () => {
+    return <div>some content</div>
 }
 
 const CanvasMain = () => {
+    return <div />
+}
 
+const RightComponentPanel = () => {
     return <div />
 }
 
 const Raid: NextPage = () => {
     return (
-        <div className="max-width-7xl m-auto    ">
+        <div className="max-w-7xl m-auto h-screen base-100 p-4">
             <Link href="/">Home</Link>
-            <ComponentPanel />
-            <CanvasMain />
-            <h1>First Post</h1>
-            <h1>Second Post</h1>
-            {
-                waymarks.filter(x => Number(x) > 1).map(x => <p>{Number(x) * 2 + 2}</p>)
-            }
+            <div className="grid grid-cols-8 py-4 gap-2 h-4/6">
+                <div className="col-span-2 card bg-neutral shadow-xl p-4">
+                    <LeftComponentPanel />
+                </div>
+                <div className="col-span-4 card bg-neutral shadow-xl p-4">
+                    <CanvasMain />
+                </div>
+                <div className="col-span-2 card bg-neutral shadow-xl p-4">
+                    <RightComponentPanel />
+                    <h1>First Post</h1>
+                    <h1>Second Post</h1>
+                    {waymarks.filter(x => Number(x) > 1).map(x => <p>{Number(x) * 2 + 2}</p>)}
+                </div>
+
+            </div>
+
         </div>
     );
 }
